abc80c60091bd058a5f182b5263af08cd2113c79
suyane924 <suyanemk@outlook.com>
2025-09-05 21:05:27 -0300

    commit

--- test_app.py
+++ test_app.py
@@ -1,5 +1,5 @@
-from app import soma
-from app import multiplica 
+from app import soma, multiplica
+
 
 def test_soma():
     assert soma(2, 3) == 5
@@ -7,12 +7,18 @@ def test_soma():
     assert soma(0, 0) == 0
 
 
-def test_multiplica_inteiros():
+def test_multiplica_positivos():
     assert multiplica(2, 3) == 6
 
-def test_multiplica_zero():
-    assert multiplica(0, 5) == 0
 
 def test_multiplica_negativos():
+    assert multiplica(-2, -3) == 6
+
+
+def test_multiplica_positivo_negativo():
     assert multiplica(-2, 3) == -6
-    
+
+
+def test_multiplica_por_zero():
+    assert multiplica(0, 5) == 0
+    assert multiplica(5, 0) == 0
